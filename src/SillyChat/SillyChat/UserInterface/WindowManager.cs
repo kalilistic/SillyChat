9ac80f37959ed5c5fb077c13d7378b86a928f38a
kalilistic <35899782+kalilistic@users.noreply.github.com>
2021-09-01 20:34:02 -0400

    refactor: update to api4

--- src/SillyChat/SillyChat/UserInterface/WindowManager.cs
+++ src/SillyChat/SillyChat/UserInterface/WindowManager.cs
@@ -1,10 +1,8 @@
-using System;
 using System.Numerics;
 
 using CheapLoc;
 using Dalamud.Game.Command;
 using Dalamud.Interface.Windowing;
-using Dalamud.Plugin;
 using ImGuiNET;
 
 namespace SillyChat
@@ -18,11 +16,9 @@ namespace SillyChat
         /// Initializes a new instance of the <see cref="WindowManager"/> class.
         /// </summary>
         /// <param name="plugin">SillyChat plugin.</param>
-        /// <param name="pluginInterface">Plugin interface.</param>
-        public WindowManager(ISillyChatPlugin plugin, DalamudPluginInterface pluginInterface)
+        public WindowManager(ISillyChatPlugin plugin)
         {
             this.Plugin = plugin;
-            this.PluginInterface = pluginInterface;
 
             // create windows
             this.ConfigWindow = new ConfigWindow(this.Plugin) { Size = new Vector2(180, 130) };
@@ -33,8 +29,8 @@ namespace SillyChat
             };
 
             // setup events
-            this.PluginInterface.UiBuilder.OnBuildUi += this.OnBuildUi;
-            this.PluginInterface.UiBuilder.OnOpenConfigUi += this.OnOpenConfigUi;
+            SillyChatPlugin.PluginInterface.UiBuilder.Draw += this.OnBuildUi;
+            SillyChatPlugin.PluginInterface.UiBuilder.OpenConfigUi += this.OnOpenConfigUi;
 
             // setup window system
             this.WindowSystem = new WindowSystem("SillyChatWindowSystem");
@@ -42,12 +38,12 @@ namespace SillyChat
             this.WindowSystem.AddWindow(this.HistoryWindow);
 
             // setup ui commands
-            this.PluginInterface.CommandManager.AddHandler("/sillyconfig", new CommandInfo(this.ToggleConfig)
+            SillyChatPlugin.CommandManager.AddHandler("/sillyconfig", new CommandInfo(this.ToggleConfig)
             {
                 HelpMessage = Loc.Localize("ConfigCommandHelp", "Show SillyChat config window."),
                 ShowInHelp = true,
             });
-            this.PluginInterface.CommandManager.AddHandler("/sillyhistory", new CommandInfo(this.ToggleHistory)
+            SillyChatPlugin.CommandManager.AddHandler("/sillyhistory", new CommandInfo(this.ToggleHistory)
             {
                 HelpMessage = Loc.Localize("HistoryCommandHelp", "Show SillyChat history window."),
                 ShowInHelp = true,
@@ -68,17 +64,15 @@ namespace SillyChat
 
         private ISillyChatPlugin Plugin { get; }
 
-        private DalamudPluginInterface PluginInterface { get; }
-
         /// <summary>
         /// Dispose plugin windows.
         /// </summary>
         public void Dispose()
         {
-            this.PluginInterface.UiBuilder.OnBuildUi -= this.OnBuildUi;
-            this.PluginInterface.UiBuilder.OnOpenConfigUi -= this.OnOpenConfigUi;
-            this.PluginInterface.CommandManager.RemoveHandler("/sillyconfig");
-            this.PluginInterface.CommandManager.RemoveHandler("/sillyhistory");
+            SillyChatPlugin.PluginInterface.UiBuilder.Draw -= this.OnBuildUi;
+            SillyChatPlugin.PluginInterface.UiBuilder.OpenConfigUi -= this.OnOpenConfigUi;
+            SillyChatPlugin.CommandManager.RemoveHandler("/sillyconfig");
+            SillyChatPlugin.CommandManager.RemoveHandler("/sillyhistory");
         }
 
         private void ToggleHistory(string command, string args)
@@ -91,7 +85,7 @@ namespace SillyChat
             this.ConfigWindow!.IsOpen ^= true;
         }
 
-        private void OnOpenConfigUi(object sender, EventArgs e)
+        private void OnOpenConfigUi()
         {
             this.ConfigWindow!.IsOpen ^= true;
         }
